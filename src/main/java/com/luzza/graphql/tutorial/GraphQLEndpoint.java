03947bf4a3d05bb148825c06466140895af85002
Jonathan Lusby <lusbyj@Jonathans-MBP.home>
2019-01-29 23:36:15 +0000

    Added mongo repo

--- src/main/java/com/luzza/graphql/tutorial/GraphQLEndpoint.java
+++ src/main/java/com/luzza/graphql/tutorial/GraphQLEndpoint.java
@@ -1,6 +1,9 @@
 package com.luzza.graphql.tutorial;
 
 import com.coxautodev.graphql.tools.SchemaParser;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
 import javax.servlet.annotation.WebServlet;
 
 import graphql.schema.GraphQLSchema;
@@ -10,14 +13,22 @@ import graphql.servlet.SimpleGraphQLServlet;
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
+    private static final LinkRepository linkRepository;
+
+    static {
+        //Change to `new MongoClient("mongodb://<host>:<port>/hackernews")`
+        //if you don't have Mongo running locally on port 27017
+        MongoDatabase mongo = new MongoClient().getDatabase("hackernews");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
+    }
+    
     public GraphQLEndpoint() {
         super(buildSchema());
     }
-    
+
     private static GraphQLSchema buildSchema() {
-        LinkRepository linkRepository = new LinkRepository();
         return SchemaParser.newParser()
-                .file("schema.graphqls") //parse the schema file created earlier
+                .file("schema.graphqls")
                 .resolvers(new Query(linkRepository), new Mutation(linkRepository))
                 .build()
                 .makeExecutableSchema();
--- src/main/java/com/luzza/graphql/tutorial/Link.java
+++ src/main/java/com/luzza/graphql/tutorial/Link.java
@@ -2,12 +2,22 @@ package com.luzza.graphql.tutorial;
 
 public class Link {
     
+    private final String id;
     private final String url;
     private final String description;
 
     public Link(String url, String description) {
+        this(null, url, description);
+    }
+
+    public Link(String id, String url, String description) {
         this.url = url;
         this.description = description;
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getUrl() {
